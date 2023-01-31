74fb5107fac3bc524cb7a65e722501552dba1f18
TESSI Hardson <hardsontessi2@gmail.com>
2023-01-31 11:42:49 +0100

    eureka server

--- zuul-server/src/test/java/com/mcommerce/zuulserver/ZuulServerApplicationTests.java
+++ zuul-server/src/test/java/com/mcommerce/zuulserver/ZuulServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.mconnerce.configserver;
+package com.mcommerce.zuulserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigServerApplicationTests {
+class ZuulServerApplicationTests {
 
 	@Test
 	void contextLoads() {
